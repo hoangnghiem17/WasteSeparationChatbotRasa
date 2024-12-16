5a97cab1d77345b9b6ff4bfe32e1d158ba5cc42f
hoangnghiem17 <ngghoang17@gmail.com>
2024-12-16 23:08:47 +0100

    Run Python directly in Container without setting PATH

--- start_services.sh
+++ start_services.sh
@@ -2,8 +2,8 @@
 
 # Start the Rasa NLU server in the background
 echo "Starting Rasa NLU server..."
-rasa run --enable-api --cors "*" --port 5005 &
+python -m rasa run --enable-api --cors "*" --port 5005 &
 
 # Start the Rasa Action server in the foreground
 echo "Starting Rasa Action server..."
-rasa run actions --port 5055
+python -m rasa run actions --port 5055
