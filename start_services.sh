a176424fc0b346e5c071db4c05be5d16941f313c
hoangnghiem17 <ngghoang17@gmail.com>
2024-12-17 09:38:30 +0100

    Added step to start Flask application

--- start_services.sh
+++ start_services.sh
@@ -7,3 +7,7 @@ rasa run --enable-api --cors "*" --port $PORT &
 # Start the Rasa Action server on port 5055
 echo "Starting Rasa Action server..."
 rasa run actions --port 5055
+
+# Start the Flask application
+echo "Starting Flask application..."
+python /app/rasa_app/app.py
