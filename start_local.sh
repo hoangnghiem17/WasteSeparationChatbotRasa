8a08a60265436cea594d3875a77a284ec27250a9
hoangnghiem17 <ngghoang17@gmail.com>
2024-12-21 13:58:58 +0100

    Automated local deployment with grog using bash script

--- start_local.sh
+++ start_local.sh
@@ -9,5 +9,10 @@ rasa run actions &
 echo "Starting Flask Application..."
 python rasa_app/app.py &
 
-# Wait for all background processes to finish
-wait
+# Wait until Flask (port 8000) is up using Python
+while ! python -c "import socket; s = socket.socket(); s.settimeout(1); print(s.connect_ex(('localhost', 8000)) == 0)" | grep -q True; do
+  sleep 1
+done
+
+echo "Flask is up. Starting Ngrok..."
+D:/ngrok-v3-stable-windows-amd64/ngrok http 8000
